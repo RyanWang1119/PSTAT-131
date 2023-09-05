240e352a38c42c5c95bc99c2b2cdb045ac16a337
RyanWang1119 <enrui_wang@ucsb.edu>
2023-09-05 20:43:10 +0800

    first commit

--- eda.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(here)
-nba_15<-read.csv(here('data', 'nba_2015-16.csv'))
-
-library(corrplot) 
-num <- nba_15[,sapply(nba_15,is.numeric)] 
- 
-corrplot(cor(num), method = 'number', type = 'lower') 
